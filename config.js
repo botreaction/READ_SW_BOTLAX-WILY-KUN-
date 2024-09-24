afb29cb4c672ad4f4bc740c6b4d34d72cb9fe4fc
wily <103911498+botreaction@users.noreply.github.com>
2024-09-24 21:01:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const config = {
-	name: 'Wily Kun', // Ubah Nama kamu [ ini nama yg dimunculin saat terkoneksi ke WA web ]
-	owner: '6289688206739', // Ubah Nomor ke Nomor owner
-	autoread: true, // Ubah Ini untuk read sw == true, untuk tidak maka == false
-	faston: 1000, // Semakin dikit nilai nya, maka semakin cepat read SW
-    pairingNumber: "6289688206739", // nomer pairing 
+  name: 'Wily Kun', // Ubah Nama kamu [ ini nama yg dimunculin saat terkoneksi ke WA web ]
+  owner: '6289688206739', // Ubah Nomor ke Nomor owner
+  autoread: true, // Ubah Ini untuk read sw == true, untuk tidak maka == false
+  faston: 1000, // Semakin dikit nilai nya, maka semakin cepat read SW
+  pairingNumber: "6289688206739", // nomer pairing 
+  uptimeBio: false // Add this line
 }
 export default config;
