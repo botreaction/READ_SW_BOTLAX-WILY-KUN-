42f7196e7a639b504b03dd691947072ab702e75e
wily <103911498+botreaction@users.noreply.github.com>
2024-09-24 21:17:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const config = {
   autoread: true, // Ubah Ini untuk read sw == true, untuk tidak maka == false
   faston: 1000, // Semakin dikit nilai nya, maka semakin cepat read SW
   pairingNumber: "6289688206739", // nomer pairing 
-  uptimeBio: false // false mati true hidup
+  uptimeBio: false // false mati true hidup fitur ini cuma melihat waktu bot berjalannya di bio wa mu
 }
 export default config;
